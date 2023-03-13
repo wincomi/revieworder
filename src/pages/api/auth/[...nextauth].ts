0274ad89ca744f72d9b173a820d7794ffc9253a6
COMI <wincomi@me.com>
2023-03-13 11:42:19 +0900

    [DB] PrismaAdapter 관련 코드 추가

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,13 @@
 import NextAuth, { AuthOptions } from 'next-auth'
 import AppleProvider from 'next-auth/providers/apple'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from "../../../utils/prismadb"
 
 const APPLE_ID = process.env.NEXTAUTH_APPLE_ID!
 const APPLE_SECRET = process.env.NEXTAUTH_APPLE_SECRET!
 
 export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         AppleProvider({
             clientId: APPLE_ID,
