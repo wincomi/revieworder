71ac6c0be25dc219f88de79f502cdcf7b23e9070
COMI <wincomi@me.com>
2023-03-15 22:41:25 +0900

    로그인 페이지 디자인 개선

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,6 +1,8 @@
 import { getProviders, signIn } from 'next-auth/react'
-import { Button, Spacer } from '@nextui-org/react'
+import { Button, Text, Spacer } from '@nextui-org/react'
 import { GetServerSideProps } from 'next'
+import { SiApple, SiNaver, SiKakaotalk, SiFacebook } from 'react-icons/si'
+import Layout from '@/components/layout'
 
 // interface SignInPage {
 //     providers: any
@@ -9,15 +11,24 @@ import { GetServerSideProps } from 'next'
 export default (/* { providers }: SignInPage */) => {
     // TODO: 로그인되어 있을 경우 return (getSession() 사용)
     return (
-        <>
-            <Button flat onPress={() => signIn("apple")}>
+        <Layout>
+            <Text h2 css={{textAlign: 'center'}}>로그인</Text>
+            <Button flat color="secondary" size="lg" onPress={() => signIn("apple")} icon={<SiApple />} css={{mx: 'auto'}}>
                 Apple 로그인
             </Button>
             <Spacer />
-            <Button flat color="warning" onPress={() => signIn("kakao")}>
+            <Button flat color="warning" size="lg" onPress={() => signIn("kakao")} icon={<SiKakaotalk />} css={{mx: 'auto'}}>
                 카카오 로그인
             </Button>
-        </>
+            <Spacer />
+            <Button flat color="success" size="lg" onPress={() => signIn("naver")} icon={<SiNaver />} css={{mx: 'auto'}}>
+                네이버 로그인
+            </Button>
+            <Spacer />
+            <Button flat color="primary" size="lg" onPress={() => signIn("naver")} icon={<SiFacebook />} css={{mx: 'auto'}}>
+                페이스북 로그인
+            </Button>
+        </Layout>
     )
 }
   
