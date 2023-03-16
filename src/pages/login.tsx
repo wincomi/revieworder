145b67d55b533ca90e49127548d0eab77af3f483
COMI <wincomi@me.com>
2023-03-16 21:06:23 +0900

    로그인 페이지에 인스타그램, 트위터 버튼 추가

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
 import { getProviders, signIn } from 'next-auth/react'
 import { Button, Text, Spacer } from '@nextui-org/react'
 import { GetServerSideProps } from 'next'
-import { SiApple, SiNaver, SiKakaotalk, SiFacebook } from 'react-icons/si'
+import { SiApple, SiNaver, SiKakaotalk, SiFacebook, SiInstagram, SiTwitter } from 'react-icons/si'
 import Layout from '@/components/layout'
 
 // interface SignInPage {
@@ -13,21 +13,30 @@ export default (/* { providers }: SignInPage */) => {
     return (
         <Layout>
             <Text h2 css={{textAlign: 'center'}}>로그인</Text>
-            <Button flat color="secondary" size="lg" onPress={() => signIn("apple")} icon={<SiApple />} css={{mx: 'auto'}}>
-                Apple 로그인
+            <Button color="primary" size="lg" onPress={() => signIn("facebook")} icon={<SiFacebook />} css={{mx: 'auto'}}>
+                페이스북 로그인
             </Button>
             <Spacer />
-            <Button flat color="warning" size="lg" onPress={() => signIn("kakao")} icon={<SiKakaotalk />} css={{mx: 'auto'}}>
+            <Button color="error" size="lg" onPress={() => signIn("instagram")} icon={<SiInstagram />} css={{mx: 'auto'}}>
+                인스타그램 로그인
+            </Button>
+            <Spacer />
+            <Button flat color="primary" size="lg" onPress={() => signIn("twitter")} icon={<SiTwitter />} css={{mx: 'auto'}} disabled>
+                트위터 로그인
+            </Button>
+            <Spacer y={2} />
+            <Button color="warning" size="lg" onPress={() => signIn("kakao")} icon={<SiKakaotalk />} css={{mx: 'auto'}}>
                 카카오 로그인
             </Button>
             <Spacer />
-            <Button flat color="success" size="lg" onPress={() => signIn("naver")} icon={<SiNaver />} css={{mx: 'auto'}}>
+            <Button color="success" size="lg" onPress={() => signIn("naver")} icon={<SiNaver />} css={{mx: 'auto'}}>
                 네이버 로그인
             </Button>
             <Spacer />
-            <Button flat color="primary" size="lg" onPress={() => signIn("facebook")} icon={<SiFacebook />} css={{mx: 'auto'}}>
-                페이스북 로그인
+            <Button color="secondary" size="lg" onPress={() => signIn("apple")} icon={<SiApple />} css={{mx: 'auto'}}>
+                Apple 로그인
             </Button>
+            <Spacer />
         </Layout>
     )
 }
