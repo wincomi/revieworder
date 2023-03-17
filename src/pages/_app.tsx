a11209b1ef89bceaecde63968a57d61fc309461b
COMI <wincomi@me.com>
2023-03-17 18:17:51 +0900

    NextUI 테마 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
-import { NextUIProvider } from '@nextui-org/react'
+import { createTheme, NextUIProvider } from '@nextui-org/react'
+
+const lightTheme = createTheme({
+  type: 'light'
+})
+
+const darkTheme = createTheme({
+  type: 'dark'
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <NextUIProvider>
+      <NextUIProvider theme={darkTheme}>
         <Component {...pageProps} />
       </NextUIProvider>
     </SessionProvider>
