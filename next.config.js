4ef1ef25310832257ecdad93d9556f32d20f821c
Yyhis <oouoo717@gmail.com>
2023-03-18 20:41:26 +0900

    API users 외부접근 막기(redirect)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // API URL 접근 시 redirect 
+  async redirects() {
+    return [
+      {
+        source: '/api/users',
+        destination: '/login',
+        permanent: false,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
