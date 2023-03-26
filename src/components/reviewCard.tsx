a3187e1ebf33ea59cd0dcb67fb7d90e69f2043e3
Yyhis <oouoo717@gmail.com>
2023-03-26 17:31:21 +0900

    메인페이지 및 리뷰카드 DB 연결

--- src/components/reviewCard.tsx
+++ src/components/reviewCard.tsx
@@ -1,33 +1,48 @@
-import { User, Card,  Row, Text, Button, Spacer, Link, Tooltip } from "@nextui-org/react"
+import { User, Card, Row, Text, Button, Spacer, Link, Tooltip } from "@nextui-org/react"
 import { FaHeart, FaShoppingCart, FaStar, FaRegStar } from 'react-icons/fa'
 
 import { useRouter } from 'next/router'
 import { useState } from "react"
+import { Prisma } from "@prisma/client"
+import dateFormat from "@/utils/dateFormat"
 
-interface ReviewCardProps {
-    // TODO
-}
+type ReviewCardProps =
+    Prisma.ReviewGetPayload<{
+        include: {
+            order: {
+                include: {
+                    store: true,
+                    user: true,
+                    orderDetail: {
+                        include: { menu: true }
+                    }
+                }
+            }
+        }
+    }>
 
-export default (props: ReviewCardProps) => {
+export default (review: ReviewCardProps) => {
     const router = useRouter()
     const [favorite, setFavorite] = useState(0)
+
+    console.log(review)
 
     return (
         <Card variant="flat">
             <Card.Header>
                 <Row wrap="wrap" justify="space-between" align="center">
-                    <User 
-                        src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                        name="이름 없음"
+                    <User
+                        src={review.order.user.image ? review.order.user.image : "https://i.pravatar.cc/150?u=a042581f4e29026704d"}
+                        name={review.order.user.name ? review.order.user.name : "이름 없음"}
                         size="sm"
-                        css={{px: 0}}
+                        css={{ px: 0 }}
                     />
-                    <Text css={{ color: "$accents7", fontSize: "$xs" }}>2023. 03. 15{/* review.createTime */}</Text>
+                    <Text css={{ color: "$accents7", fontSize: "$xs" }}>{dateFormat(review.createTime)}</Text>
                 </Row>
             </Card.Header>
             <Card.Body css={{ p: 0 }}>
                 <Card.Image
-                    src="https://source.unsplash.com/random/600x600/?food"
+                    src={review.image ? review.image : "https://source.unsplash.com/random/600x600/?food"}
                     objectFit="cover"
                     width="100%"
                     height={300}
@@ -35,7 +50,7 @@ export default (props: ReviewCardProps) => {
                 />
             </Card.Body>
             <Card.Footer css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
-                <div style={{ width: '100%'}}>
+                <div style={{ width: '100%' }}>
                     <Row wrap="wrap" justify="space-between" align="center">
                         <Tooltip content={"리뷰 추천하기"} placement="top" color="error">
                             <Button auto flat size="xs" color="error" icon={<FaHeart />} onPress={() => setFavorite(favorite + 1)}>
@@ -43,28 +58,30 @@ export default (props: ReviewCardProps) => {
                             </Button>
                         </Tooltip>
                         <Tooltip content={"리뷰어가 선택한 별점"} placement="top" color="warning">
-                            <Button auto flat size="xs" color="warning" icon={<><FaStar /><Spacer x={0.1} /><FaStar /><Spacer x={0.1} /><FaStar /><Spacer x={0.1} /><FaStar /><Spacer x={0.1} /><FaRegStar /></>}>(4/5)</Button>
+                            <Button auto flat size="xs" color="warning" icon={<><FaStar /><Spacer x={0.1} /><FaStar /><Spacer x={0.1} /><FaStar /><Spacer x={0.1} /><FaStar /><Spacer x={0.1} /><FaRegStar /></>}>({review.rating}/5)</Button>
                         </Tooltip>
                     </Row>
                     <Spacer y={0.5} />
 
-                    국회는 국무총리 또는 국무위원의 해임을 대통령에게 건의할 수 있다. 광물 기타 중요한 지하자원·수산자원·수력과 경제상 이용할 수 있는 자연력은 법률이 정하는 바에 의하여 일정한 기간 그 채취·개발 또는 이용을 특허할 수 있다.<br />
-                    모든 국민은 법률이 정하는 바에 의하여 선거권을 가진다. 국정감사 및 조사에 관한 절차 기타 필요한 사항은 법률로 정한다. 형사피해자는 법률이 정하는 바에 의하여 당해 사건의 재판절차에서 진술할 수 있다.
+                    {review.content}
+                    {/* 국회는 국무총리 또는 국무위원의 해임을 대통령에게 건의할 수 있다. 광물 기타 중요한 지하자원·수산자원·수력과 경제상 이용할 수 있는 자연력은 법률이 정하는 바에 의하여 일정한 기간 그 채취·개발 또는 이용을 특허할 수 있다.<br />
+                    모든 국민은 법률이 정하는 바에 의하여 선거권을 가진다. 국정감사 및 조사에 관한 절차 기타 필요한 사항은 법률로 정한다. 형사피해자는 법률이 정하는 바에 의하여 당해 사건의 재판절차에서 진술할 수 있다. */}
 
-                    <Text h6 css={{mt: "$4", mb: "$2"}}>주문한 가게</Text>
+                    <Text h6 css={{ mt: "$4", mb: "$2" }}>주문한 가게</Text>
                     <Tooltip content={"리뷰어가 주문한 가게로 이동하기"} placement="right" color="invert">
-                        <Link href={`/store/0`} color="inherit">중국집</Link>
+                        <Link href={`/store/0`} color="inherit">{review.order.store.name}</Link>
                     </Tooltip>
-
-                    <Text h6 css={{mt: "$4", mb: "$2"}}>주문한 메뉴</Text>
-                    <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
-                        - 짜장면 &times; 1<br />
-                        - 짬뽕 &times; 1<br />
-                        - 탕수육 &times; 1<br />
-                    </Text>
                     <Spacer y={0.5} />
+                    <Text h6 css={{ mt: "$4", mb: "$2" }}>주문한 메뉴</Text>
                     
-                    <Button css={{width: '100%'}} onPress={() => router.push(`/cart`)} color="gradient" icon={<FaShoppingCart />}>
+                    {(review.order.orderDetail)?.map((item) => (
+                        <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
+                            - {item.menu.name} &times; {item.count}<br />
+                        </Text>
+                    ))}
+                    <Spacer y={0.5} />
+
+                    <Button css={{ width: '100%' }} onPress={() => router.push(`/cart`)} color="gradient" icon={<FaShoppingCart />}>
                         이 메뉴로 주문하기
                     </Button>
                 </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,34 @@ import { useRouter } from 'next/router'
 import Layout from '@/components/layout'
 import ReviewCard from '@/components/reviewCard'
 import { HiSearch } from 'react-icons/hi'
+import { Prisma } from '@prisma/client'
+import { GetServerSideProps } from 'next/types'
+import { Key } from 'react'
 
-export default function Home() {
+interface reviewProps {
+    // review에 연결된 order에서 store,user,orderDetail을 다시 orderDetail에서 menu정보를 불러온다
+    // 결론: review에 연결된 모든 테이블 조회 가능
+    // + 나중에 타입들 모아서 라이브러리화 생각 중.
+    review: Prisma.ReviewGetPayload<{
+        include: { 
+            order: {
+                include: {
+                    store: true,
+                    user: true, 
+                    orderDetail: {
+                        include: {menu: true}
+                    }
+                }
+            }
+        }   
+    }>
+}
+
+export default function Home ( { review }: reviewProps ) {
     const session = useSession()
     const router = useRouter()
+
+    console.log(review)
 
     function SearchInput() {
         return (
@@ -22,11 +46,9 @@ export default function Home() {
             />
         )
     }
-
-    const list = [1, 2, 3, 4, 5] // Sample Data
-
+    
     return (
-        <>
+        <>        
             <Head>
                 <title>리뷰오더</title>
             </Head>
@@ -37,13 +59,24 @@ export default function Home() {
                     <Grid css={{ml: '$4'}}><Button auto flat css={{h: '100%'}} icon={<HiSearch />}>검색</Button></Grid>
                 </Grid.Container>
                 <Grid.Container gap={2} justify="flex-start" css={{px: 0}}>
-                    {list.map((item, index) => (
+                    {(review as unknown as reviewProps[])?.map((item: reviewProps, index: Key) => (
                         <Grid xs={12} sm={6} lg={4} key={index}>
-                            <ReviewCard />
+                            {/*props 2개 인 이유는 review의 기본컬럼이랑 연결된 테이블들을 따로 인식돼서 2개 다 주었음*/}
+                            {<ReviewCard {...item.review} {...item} />}
                         </Grid>
                     ))}
                 </Grid.Container>
             </Layout>
         </>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+    const result = await fetch(`${process.env.NEXTAUTH_URL}/api/reviews`, { method: "GET" })
+    const review = await result.json().then(data => data as reviewProps) // any to reviewProps
+
+    return {
+        props: { review } 
+    }
 }
