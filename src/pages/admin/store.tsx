2238a5f7ba871e03289f253e0b97c02b33bd1c7a
Yyhis <oouoo717@gmail.com>
2023-05-19 14:11:48 +0900

    admin/store 페이지 추가

---
+++ src/pages/admin/store.tsx
@@ -0,0 +1,7 @@
+import Layout from '@/components/admin/layout'
+
+// TODO: 메뉴 표시 + selection 사용 (처음에는 가지고 있는 첫 매장으로 이동 이후 셀렉터로 이동) + 메뉴 관리로 이동 + 매장 설정 변경
+
+export default () => {
+    return <Layout>매장 관리</Layout>
+}
