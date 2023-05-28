69b96f2ba18a2472b5fc29ebe28e0e69a0ded390
Yyhis <oouoo717@gmail.com>
2023-05-28 13:40:41 +0900

    layout 매장 추가

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 import Navbar, { NavbarMenuItem } from '@/components/_navbar/navbar'
 import UserButton from '@/components/_navbar/userButton'
 import SettingsButton from '@/components/_navbar/settingsButton'
-import { FaHome, FaShoppingCart, FaUser } from 'react-icons/fa'
+import { FaHome, FaShoppingCart, FaStore, FaUser } from 'react-icons/fa'
 import { ImSpoonKnife } from 'react-icons/im'
 
 interface LayoutProps {
@@ -13,6 +13,7 @@ interface LayoutProps {
 export default ({ children }: LayoutProps) => {
     const menu: NavbarMenuItem[] = [
         { id: 'index', name: '둘러보기', path: '/', icon: <FaHome /> },
+        { id: 'store', name: '매장', path: '/store', icon: <FaStore /> },
         { id: 'cart', name: '주문하기', path: '/cart', icon: <FaShoppingCart /> },
         { id: 'profile', name: '내 프로필', path: '/profile', icon: <FaUser /> },
     ]
