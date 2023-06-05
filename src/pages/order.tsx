4a7b181bd5dcca65ce7ed417d6a3af0fabe06e4a
Yyhis <oouoo717@gmail.com>
2023-06-05 17:00:19 +0900

    리뷰 작성 버튼 활성화

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -2,18 +2,21 @@ import Layout from '@/components/layout'
 import { Button, Card, Grid, Text } from '@nextui-org/react'
 import { GetServerSideProps } from 'next/types'
 import { OrderAPIGETResponse, OrderItem } from './api/orders'
-import { Menu, OrderDetail } from '@prisma/client'
+import { Menu, OrderDetail, Review } from '@prisma/client'
 import orderStatusFormat from '@/utils/orderStatusFormat'
 import router from 'next/router'
+import { ReviewAPIGETResponse } from './api/reviews'
 
 interface OrderPageProps {
+    /// 내 리뷰
+    reviews: Review[]
     /// 주문 내역
     orderItems: OrderItem[]
 }
 
 type orderMenu = OrderDetail & { menu: Menu }
 
-export default ({ orderItems }: OrderPageProps) => {
+export default ({ reviews, orderItems }: OrderPageProps) => {
     const sumPrice = (order: OrderItem) => {
         const totalPrice = order.orderDetails.reduce((totalPrice, item) => {
             return totalPrice + item.menu.price * item.amount
@@ -31,6 +34,22 @@ export default ({ orderItems }: OrderPageProps) => {
         )
     }
 
+    // 리뷰 연결이 안 되있다.
+    console.log(orderItems)
+
+    const checkReview = (orderId: string) => {
+        if (reviews == undefined) {
+            return false
+        } else {
+            for (let i = 0; i < reviews.length; i++) {
+                if (reviews[i].orderId == orderId) {
+                    return true
+                }
+            }
+            return false
+        }
+    }
+
     return (
         <Layout>
             <title>My 주문</title>
@@ -42,9 +61,13 @@ export default ({ orderItems }: OrderPageProps) => {
                                 <Grid>
                                     <Text h5>주문 내역{index + 1}</Text>
                                     <Text>{orderStatusFormat(order.status)}</Text>
-                                    <Button onPress={() => router.push(`/reviewEnroll?orderId=${order.id}`)}>
-                                        리뷰 작성
-                                    </Button>
+                                    {checkReview(order.id) ? (
+                                        <Button disabled>리뷰 작성</Button>
+                                    ) : (
+                                        <Button onPress={() => router.push(`/reviewEnroll?orderId=${order.id}`)}>
+                                            리뷰 작성
+                                        </Button>
+                                    )}
                                 </Grid>
                             </Card.Header>
                             <Card.Divider></Card.Divider>
@@ -72,7 +95,7 @@ export default ({ orderItems }: OrderPageProps) => {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     // storeId 해당 매장 주문들 정보
-    const result = await fetch(`${process.env.NEXTAUTH_URL}/api/admin/orders`, {
+    const result = await fetch(`${process.env.NEXTAUTH_URL}/api/orders`, {
         method: 'GET',
         headers: {
             // session의 쿠키 전달
@@ -83,7 +106,18 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     const response = await result.json().then((data) => data as OrderAPIGETResponse)
     const orderItems = response.data
 
+    const reviewResult = await fetch(`${process.env.NEXTAUTH_URL}/api/reviews`, {
+        method: 'GET',
+        headers: {
+            // session의 쿠키 전달
+            cookie: context.req.headers.cookie || '',
+        },
+    })
+
+    const reviewRes = await reviewResult.json().then((data) => data as ReviewAPIGETResponse)
+    const reviews = reviewRes.data
+
     return {
-        props: { orderItems },
+        props: { reviews, orderItems },
     }
 }
