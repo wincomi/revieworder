7f9105eaccc2204025feddf55febafe6b51846b4
COMI <wincomi@me.com>
2023-06-07 14:20:56 +0900

    매장 페이지에 QR코드 추가

--- src/pages/stores/[store_id].tsx
+++ src/pages/stores/[store_id].tsx
@@ -1,17 +1,23 @@
 import Layout from '@/components/layout'
-import { Grid, Text, Card, Spacer, Button, Loading, Tooltip, Row } from '@nextui-org/react'
+import { Grid, Text, Card, Spacer, Button, Loading, Row, Modal } from '@nextui-org/react'
 import { GetServerSideProps } from 'next/types'
 import { MenuAPIGETResponse, MenuItem } from '../api/menus'
 import { Menu } from '@prisma/client'
-import router from 'next/router'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { FaShoppingCart } from 'react-icons/fa'
+import QRCode from 'react-qr-code'
+import Head from 'next/head'
+import { HiQrcode } from 'react-icons/hi'
 
 interface StorePageProps {
     menuItems: MenuItem[]
 }
 
 export default ({ menuItems }: StorePageProps) => {
+    const router = useRouter()
+    const [visibleQRCode, setVisibleQRCode] = useState(false)
+
     const [isAddingToCart, setIsAddingToCart] = useState(false)
 
     const addToCart = async (index: number) => {
@@ -48,56 +54,70 @@ export default ({ menuItems }: StorePageProps) => {
     }
 
     return (
-        <Layout>
-            <Text h3>{menuItems[0].store.name}</Text>
+        <>
+            <Head>
+                <title>{menuItems[0].store.name}</title>
+            </Head>
 
-            <Grid.Container gap={2}>
-                <Grid sm={12}>
-                    {menuItems.map((item: Menu, index) => (
-                        <>
-                            <Card key={index} variant="flat">
-                                <Card.Header>
-                                    <Row wrap="wrap" justify="space-between" align="center">
-                                        <Text h3 css={{ mb: 0 }}>
-                                            {item.name}
-                                        </Text>
-                                        <Text h4 css={{ mb: 0 }}>
-                                            {item.price.toLocaleString()}원
-                                        </Text>
-                                    </Row>
-                                </Card.Header>
-                                <Card.Body css={{ p: 0, flexGrow: 'unset' }}>
-                                    <Card.Image
-                                        src={item.image ?? 'http://via.placeholder.com/480x480'}
-                                        objectFit="cover"
-                                        width="100%"
-                                        height={300}
-                                        alt="매장 사진"
-                                        showSkeleton
-                                    />
-                                </Card.Body>
-                                <Card.Footer>
-                                    <Button
-                                        css={{ width: '100%' }}
-                                        color="gradient"
-                                        icon={<FaShoppingCart />}
-                                        onPress={async () => await addToCart(index)}
-                                        disabled={isAddingToCart}
-                                    >
-                                        {isAddingToCart ? (
-                                            <Loading type="points" color="currentColor" size="sm" />
-                                        ) : (
-                                            <>장바구니에 담기</>
-                                        )}
-                                    </Button>
-                                </Card.Footer>
-                            </Card>
-                            <Spacer y={0.5} />
-                        </>
-                    ))}
-                </Grid>
-            </Grid.Container>
-        </Layout>
+            <Layout>
+                <Text h1>{menuItems[0].store.name}</Text>
+                <Button onPress={() => setVisibleQRCode(true)} icon={<HiQrcode />} flat css={{ mb: 12 }}>
+                    매장 QR코드 보기
+                </Button>
+                <Modal open={visibleQRCode} onClose={() => setVisibleQRCode(false)}>
+                    <Modal.Body>
+                        <QRCode value={document.URL} size={400} style={{ width: '100%' }} />
+                    </Modal.Body>
+                </Modal>
+
+                <Grid.Container>
+                    <Grid sm={12}>
+                        {menuItems.map((item: Menu, index) => (
+                            <>
+                                <Card key={index} variant="flat">
+                                    <Card.Header>
+                                        <Row wrap="wrap" justify="space-between" align="center">
+                                            <Text h3 css={{ mb: 0 }}>
+                                                {item.name}
+                                            </Text>
+                                            <Text h4 css={{ mb: 0 }}>
+                                                {item.price.toLocaleString()}원
+                                            </Text>
+                                        </Row>
+                                    </Card.Header>
+                                    <Card.Body css={{ p: 0, flexGrow: 'unset' }}>
+                                        <Card.Image
+                                            src={item.image ?? 'http://via.placeholder.com/480x480'}
+                                            objectFit="cover"
+                                            width="100%"
+                                            height={300}
+                                            alt="매장 사진"
+                                            showSkeleton
+                                        />
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <Button
+                                            css={{ width: '100%' }}
+                                            color="gradient"
+                                            icon={<FaShoppingCart />}
+                                            onPress={async () => await addToCart(index)}
+                                            disabled={isAddingToCart}
+                                        >
+                                            {isAddingToCart ? (
+                                                <Loading type="points" color="currentColor" size="sm" />
+                                            ) : (
+                                                <>장바구니에 담기</>
+                                            )}
+                                        </Button>
+                                    </Card.Footer>
+                                </Card>
+                                <Spacer y={0.5} />
+                            </>
+                        ))}
+                    </Grid>
+                </Grid.Container>
+            </Layout>
+        </>
     )
 }
 
